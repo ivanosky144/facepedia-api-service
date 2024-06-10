487c1f23b1ff61739efd724bc4f6492843a344c1
Joy Ivan <ivanoskytan@gmail.com>
2024-06-10 11:14:55 +0700

    feat: add community handlers

--- handlers/community_handlers.go
+++ handlers/community_handlers.go
@@ -3,8 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/temuka-api-service/models"
+	"gorm.io/gorm"
 )
 
 func CreateCommunity(w http.ResponseWriter, r *http.Request) {
@@ -39,5 +42,61 @@ func CreateCommunity(w http.ResponseWriter, r *http.Request) {
 }
 
 func JoinCommunity(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	communityIDstr := vars["id"]
+
+	communityID, err := strconv.Atoi(communityIDstr)
+	if err != nil {
+		http.Error(w, "Invalid community id", http.StatusBadRequest)
+		return
+	}
+
+	var requestBody struct {
+		UserID int `json:"user_id"`
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var community models.Community
+	if err := db.First(&community, communityID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			http.Error(w, "Community not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Error retrieving community", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	isMember := false
+	for _, memberID := range community.Members {
+		if memberID == requestBody.UserID {
+			isMember = true
+			break
+		}
+	}
+
+	if isMember {
+		http.Error(w, "User already a member of the community", http.StatusBadRequest)
+		return
+	}
+
+	community.Members = append(community.Members, requestBody.UserID)
+	community.MembersCount += 1
+
+	if err := db.Save(&community).Error; err != nil {
+		http.Error(w, "Error updating community", http.StatusInternalServerError)
+		return
+	}
+
+	response := struct {
+		Message string `json:"message"`
+	}{
+		Message: "Successfully joined the community",
+	}
+	json.NewEncoder(w).Encode(response)
 
 }
--- models/community.go
+++ models/community.go
@@ -8,15 +8,16 @@ import (
 
 type Community struct {
 	gorm.Model
-	ID          int       `gorm:"primary_key;column:id"`
-	Name        string    `gorm:"column:name"`
-	Description string    `gorm:"column:desc"`
-	Rules       []string  `gorm:"column:rules"`
-	Members     int       `gorm:"column:members"`
-	Moderators  []int     `gorm:"column:moderators"`
-	LogoPicture string    `gorm:"column:logo_picture"`
-	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	ID           int       `gorm:"primary_key;column:id"`
+	Name         string    `gorm:"column:name"`
+	Description  string    `gorm:"column:desc"`
+	Rules        []string  `gorm:"column:rules"`
+	Members      []int     `gorm:"column:members"`
+	MembersCount int       `gorm:column:members_count`
+	Moderators   []int     `gorm:"column:moderators"`
+	LogoPicture  string    `gorm:"column:logo_picture"`
+	CreatedAt    time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt    time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
 }
 
 func (c *Community) TableName() string {
