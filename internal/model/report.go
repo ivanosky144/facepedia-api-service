393eb0e43ac54e3922d7171bf6b1a0e8d68d3afa
ivanosky144 <ivanoskytan@gmail.com>
2024-08-19 20:59:17 +0700

    feat: add report model

---
+++ internal/model/report.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Report struct {
+	gorm.Model
+	ID        int       `gorm:"primary_key;column:id"`
+	PostID    *int      `gorm:"column:post_id;null"`
+	CommentID *int      `gorm:"column:comment_id;null"`
+	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdateAt  time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+}
+
+func (r *Report) TableName() string {
+	return "reports"
+}
