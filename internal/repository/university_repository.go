3d622887f1093f79c944efde5634404285dd6fef
ivanosky144 <ivanoskytan@gmail.com>
2024-08-31 15:33:24 +0700

    feat: add university repository

---
+++ internal/repository/university_repository.go
@@ -0,0 +1,54 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/temuka-api-service/internal/model"
+	"gorm.io/gorm"
+)
+
+type UniversityRepository interface {
+	CreateUniversity(ctx context.Context, university *model.University) error
+	UpdateUniversity(ctx context.Context, id int, university *model.University) error
+	GetUniversities(ctx context.Context) ([]model.University, error)
+	DeleteUniversity(ctx context.Context, id int) error
+	GetUniversityDetailByID(ctx context.Context, id int) (*model.University, error)
+}
+
+type UniversityRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewUniversityRepository(db *gorm.DB) UniversityRepository {
+	return &UniversityRepositoryImpl{
+		db: db,
+	}
+}
+
+func (r *UniversityRepositoryImpl) CreateUniversity(ctx context.Context, university *model.University) error {
+	return r.db.WithContext(ctx).Create(university).Error
+}
+
+func (r *UniversityRepositoryImpl) DeleteUniversity(ctx context.Context, id int) error {
+	return r.db.WithContext(ctx).Delete(&model.University{}, id).Error
+}
+
+func (r *UniversityRepositoryImpl) UpdateUniversity(ctx context.Context, id int, post *model.University) error {
+	return r.db.WithContext(ctx).Model(&model.University{}).Where("id = ?", id).Updates(post).Error
+}
+
+func (r *UniversityRepositoryImpl) GetUniversities(ctx context.Context) ([]model.University, error) {
+	var universities []model.University
+	if err := r.db.WithContext(ctx).Find(&universities).Error; err != nil {
+		return nil, err
+	}
+	return universities, nil
+}
+
+func (r *UniversityRepositoryImpl) GetUniversityDetailByID(ctx context.Context, id int) (*model.University, error) {
+	var university model.University
+	if err := r.db.WithContext(ctx).First(&university, id).Error; err != nil {
+		return nil, err
+	}
+	return &university, nil
+}
