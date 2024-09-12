bc6c0546b76f4e8939e24982346fc3d371324b5b
ivanosky144 <ivanoskytan@gmail.com>
2024-09-12 21:02:57 +0700

    feat: integrate S3 into file uploader

--- internal/controller/file_controller.go
+++ internal/controller/file_controller.go
@@ -1,11 +1,15 @@
 package controller
 
 import (
-	"io"
+	"context"
+	"fmt"
 	"net/http"
-	"os"
 	"path/filepath"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/temuka-api-service/config"
 	httputil "github.com/temuka-api-service/pkg/http"
 )
 
@@ -45,24 +49,27 @@ func (h *FileUploadHandler) Upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tempFilePath := filepath.Join(h.UploadDirectory, handler.Filename)
-	tempFile, err := os.Create(tempFilePath)
-	if err != nil {
-		httputil.WriteResponse(w, http.StatusInternalServerError, map[string]string{"error": "Could not create file"})
-		return
-	}
-	defer tempFile.Close()
+	s3Key := fmt.Sprintf("uploads/%s", handler.Filename)
 
-	_, err = io.Copy(tempFile, file)
+	_, err = config.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket:      aws.String(config.S3Bucket),
+		Key:         aws.String(s3Key),
+		Body:        file,
+		ContentType: aws.String(handler.Header.Get("Content-Type")),
+		ACL:         types.ObjectCannedACLPublicRead,
+	})
+
 	if err != nil {
-		httputil.WriteResponse(w, http.StatusInternalServerError, map[string]string{"error": "Could not save file"})
+		httputil.WriteResponse(w, http.StatusInternalServerError, map[string]string{"error": "Could not upload file to S3"})
 		return
 	}
 
 	response := struct {
 		Message string `json:"message"`
+		URL     string `json:"url"`
 	}{
 		Message: "File has been uploaded",
+		URL:     fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", config.S3Bucket, config.S3Client.Options().Region, s3Key),
 	}
 
 	httputil.WriteResponse(w, http.StatusOK, response)
