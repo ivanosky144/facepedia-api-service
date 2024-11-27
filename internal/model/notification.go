19143ec1e1b820085584caa81312cbd87ce4bbbb
ivanosky144 <ivanoskytan@gmail.com>
2024-11-27 23:35:50 +0700

    fix: add readed_at field in notification model

--- internal/model/notification.go
+++ internal/model/notification.go
@@ -8,16 +8,17 @@ import (
 
 type Notification struct {
 	gorm.Model
-	ID        int       `gorm:"primary_key;column:id"`
-	UserID    int       `gorm:"column:user_id"`
-	ActorID   int       `gorm:"column:actor_id"`
-	PostID    int       `gorm:"column:post_id"`
-	CommentID int       `gorm:"column:comment_id"`
-	Type      string    `gorm:"column:type"`
-	Read      bool      `gorm:"column:read;default:false"`
-	Message   string    `gorm:"column:message"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	ID        int        `gorm:"primary_key;column:id"`
+	UserID    int        `gorm:"column:user_id"`
+	ActorID   int        `gorm:"column:actor_id"`
+	PostID    int        `gorm:"column:post_id"`
+	CommentID int        `gorm:"column:comment_id"`
+	Type      string     `gorm:"column:type"`
+	Read      bool       `gorm:"column:read;default:false"`
+	Message   string     `gorm:"column:message"`
+	CreatedAt time.Time  `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt time.Time  `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	ReadedAt  *time.Time `gorm:"column:readed_at;default:null"`
 }
 
 func (n *Notification) TableName() string {
