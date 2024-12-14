5980938b45dc60e40867d8c88885ff708ff5d165
ivanosky144 <ivanoskytan@gmail.com>
2024-12-14 07:35:31 +0700

    fix: set up redis in cloud

--- config/redis.go
+++ config/redis.go
@@ -3,8 +3,10 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -13,8 +15,20 @@ var (
 )
 
 func InitRedis() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Error loading env file: %v", err)
+	}
+
+	redisHost := os.Getenv("REDIS_HOST")
+	redisUser := os.Getenv("REDIS_USER")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr:     redisHost,
+		Username: redisUser,
+		Password: redisPassword,
+		DB:       0,
 	})
 
 	_, err := RedisClient.Ping(Ctx).Result()
